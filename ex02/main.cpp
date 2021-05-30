797987ab61ecc055f6ac8899e3a1a77198490a04
solareenlo <solareenlo@gmail.com>
2021-05-30 23:41:45 +0900

    update: ex02/main

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/30 22:50:14 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/05/30 23:07:15 by tayamamo         ###   ########.fr       */
+/*   Updated: 2021/05/30 23:40:00 by tayamamo         ###   ########.fr       */
 /*   Copyright 2021                                                           */
 /* ************************************************************************** */
 
@@ -14,11 +14,15 @@
 #include "Squad.hpp"
 #include "ISpaceMarine.hpp"
 #include "TacticalMarine.hpp"
+#include "AssaultTerminator.hpp"
 
 int main() {
     ISpaceMarine*   bob = new TacticalMarine;
+    ISpaceMarine*   jim = new AssaultTerminator;
 
     ISquad* vlc = new Squad;
+    vlc->push(bob);
+    vlc->push(jim);
     vlc->push(bob);
     for (int i = 0; i < vlc->getCount(); i++) {
         ISpaceMarine* cur = vlc->getUnit(i);
